72f12db0b5b33c08a9ca5ae88e530ed33481f139
wilsonchenghy <150957380+wilsonchenghy@users.noreply.github.com>
2024-03-30 15:05:13 -0400

    adding prompt input area

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
 function App() {
+
+  const [inputContent, changeInputContent] = useState("");
+
+  const enteredPrompt = () => {
+    axios.post("", {content: inputContent})
+      .then((response) => {
+        
+      })
+        .catch((error) => {console.error("Error: ", error)});
+  }
 
   return (
     <>
@@ -17,6 +28,13 @@ function App() {
       <div className="shadow-effect">
         <img src="logo.png" alt="Logo" />
       </div>
+
+      <div className="inputContainer">
+          <label>Enter Prompt: </label>
+          <input type='text' value={inputContent} onChange={(event) => changeInputContent(event.target.value)}/>
+          <button className='addButton' onClick={enteredPrompt}>Add</button>
+      </div>
+
     </>
   )
 }
