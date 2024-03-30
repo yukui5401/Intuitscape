a7da4b243a4eac6dbbbf7f627e64c409d4877f7b
wilsonchenghy <150957380+wilsonchenghy@users.noreply.github.com>
2024-03-30 15:35:15 -0400

    add input buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,37 @@ import './App.css'
 
 function App() {
 
-  const [inputContent, changeInputContent] = useState("");
+  const [inputContent, changeInputContent] = useState(" ");
 
   const enteredPrompt = () => {
     axios.post("", {content: inputContent})
       .then((response) => {
-        
+        changeInputContent(" ");
       })
         .catch((error) => {console.error("Error: ", error)});
+  }
+
+  const [selectedItem, setSelectedItem] = useState('');
+  
+  const handleChange = (e) => {
+    setSelectedItem(e.target.value);
+  };
+
+  const enteredDropdown = () => {
+
   }
 
   return (
     <>
       <h1 className="typewriter">
         <p className="typed">
-          <p className="gradient-text">Welcome to</p>
-          <p className="gradient-text2">intuitscape</p>
-          <p className="gradient-text">
-            . We make learning intuitive and easy.
-          </p>
+          <div className='gradient-text-container'>
+            <div className="gradient-text">Welcome to</div>
+            <p className="gradient-text2">intuitscape</p>
+            <p className="gradient-text">
+              . We make learning intuitive and easy.
+            </p>
+          </div>
         </p>
       </h1>
       <div className="shadow-effect">
@@ -32,8 +44,34 @@ function App() {
       <div className="inputContainer">
           <label>Enter Prompt: </label>
           <input type='text' value={inputContent} onChange={(event) => changeInputContent(event.target.value)}/>
-          <button className='addButton' onClick={enteredPrompt}>Add</button>
+          <button className='enterPromptButton' onClick={enteredPrompt}>Enter</button>
       </div>
+
+      <div>
+        <select value={selectedItem} onChange={handleChange}>
+          <option value="">Select an item</option>
+          <option value="item1">Item 1</option>
+          <option value="item2">Item 2</option>
+          <option value="item3">Item 3</option>
+        </select>
+
+        <select value={selectedItem} onChange={handleChange}>
+          <option value="">Select an item</option>
+          <option value="item1">Item 1</option>
+          <option value="item2">Item 2</option>
+          <option value="item3">Item 3</option>
+        </select>
+
+        <select value={selectedItem} onChange={handleChange}>
+          <option value="">Select an item</option>
+          <option value="item1">Item 1</option>
+          <option value="item2">Item 2</option>
+          <option value="item3">Item 3</option>
+        </select>
+
+      </div>
+
+      <button className='enterDropdownButton' onClick={enteredDropdown}>Enter dropdown</button>
 
     </>
   )
